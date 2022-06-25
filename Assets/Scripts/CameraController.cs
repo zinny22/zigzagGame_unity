1d71b7305c5078e729c6e5966e1ddcedd9527e60
zinny22 <hziny722@naver.com>
2022-06-25 17:59:41 +0900

    플레이어에 따라 카메라 이동

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private float distance;
+
+    private void Awake()
+    {
+        distance = Vector3.Distance(transform.position, target.position);
+    }
+
+    private void LateUpdate()
+    {
+        if (target == null) return;
+
+        transform.position = target.position + transform.rotation * new Vector3(0, 0, -distance);
+    }
+}
