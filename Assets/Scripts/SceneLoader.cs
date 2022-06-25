07209ba59aa4a3facaced96449baa860260b952d
zinny22 <hziny722@naver.com>
2022-06-25 20:22:46 +0900

    게임 재시작 구현 완료

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName = "")
+    {
+        if (sceneName == "")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
